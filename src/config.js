561a45099242d9187d370d5630edd90c42effd1c
turpster <turpster@turpster.net>
2020-01-05 23:53:34 +0000

    Create Config class

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+module.exports = class Config
+{
+    constructor(fileName, defaultContents)
+    {
+        if (!fs.existsSync(fileName))
+        {
+            fs.writeFileSync(fileName, defaultContents)
+        }
+
+        this.contents = require(fileName);
+    }
+};
