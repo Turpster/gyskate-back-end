cec7c58d2ace28cc47170ff9d1dca96e48f1c4f7
turpster <turpster@turpster.net>
2020-01-07 21:42:01 +0000

    Added a few more functions to the to-do list Added binary-file to dependencies to deal with binaries

--- src/database.js
+++ src/database.js
@@ -1,5 +1,33 @@
 const Database = require("./types/database/database");
 const Settings = require("./settings");
 
+const UserRecord = require('./types/database/records/user');
+const ContentRecord = require('./types/database/records/content');
+
+const BinaryFile = require("binary-file");
+
 module.exports.database = new Database(Settings.contents().database.host, Settings.contents().database.port,
     Settings.contents().database.user.username, Settings.contents().user.password, Settings.contents().database.name);
+
+
+/**
+ * create a new user
+ * @param username {string} the user's username
+ * @param displayName {string} the user's display name
+ * @param md5Password {string} the user's password md5 hash
+ * @throws Error if username is already taken
+ * @returns {UserRecord}
+ */
+module.exports.createUser = (username, displayName, md5Password) => {
+
+};
+
+/**
+ * create new content
+ * @param {BinaryFile} contentBin
+ * @param {string} contentFilename filename of content
+ * @returns {ContentRecord} the new content
+ */
+module.exports.createContent = (contentBin, contentFilename) => {
+
+};
--- src/types/database/records/user.js
+++ src/types/database/records/user.js
@@ -1,4 +1,5 @@
 const Database = require("../database");
+const Content = require('./content');
 
 module.exports = class UserRecord
 {
@@ -18,6 +19,25 @@ module.exports = class UserRecord
      * @returns {string} the display name of the user
      */
     getDisplayName()
+    {
+
+    }
+
+    /**
+     * authenticate user
+     * @param {string} md5Hash the hash of the password
+     * @return {boolean} whether password is correct or not
+     */
+    authenticate(md5Hash)
+    {
+
+    }
+
+    /**
+     * get users contents
+     * @returns {Content[]} an array of content
+     */
+    getContent()
     {
 
     }
