9dd410903d79ad8f091ea4ef1b4a2ad04bcca91e
turpster <turpster@turpster.net>
2020-01-07 19:24:35 +0000

    Added path and json compare dependencies for Config class

--- src/types/config.js
+++ src/types/config.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const DatabaseNotChanged = require("./database/database-not-changed");
+const path = require('path');
+const jsonDiff = require("json-diff").diff;
 
 module.exports = class Config
 {
@@ -7,23 +9,18 @@ module.exports = class Config
 
     constructor(fileName, defaultContents, shouldChangeOnNew=false)
     {
-
-        if (!fs.existsSync(fileName))
+        if (!fs.existsSync(file))
         {
-            fs.writeFileSync(fileName, defaultContents);
-            if (shouldChangeOnNew)
-            {
-                console.log(`Please change ${fileName} before you start this application.`);
-                throw DatabaseNotChanged(fileName);
-            }
+            fs.writeFileSync(file, JSON.stringify(defaultContents, null,4));
+            if (shouldChangeOnNew) throw DatabaseNotChanged(file)
         }
 
-        this.contents = require(fileName);
-        this.hasJsonChanged = defaultContents !== this.contents;
+        this.contents = require(path.relative(__dirname, file));
+        this.hasJsonChanged = jsonDiff(this.contents, defaultContents);
 
         if (shouldChangeOnNew && !this.hasJsonChanged)
         {
-            throw DatabaseNotChanged(fileName);
+            throw DatabaseNotChanged(file);
         }
     }
 }
